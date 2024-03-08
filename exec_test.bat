4a954a33c38dd75fe3ff33f717f8c47fb10d04f2
JackChuang <JackChunag@taiwanmobile.com>
2024-03-08 09:54:55 +0000

    remove

--- exec_test.bat
+++ exec_test.bat
@@ -1,5 +1,4 @@
 del /S /Q results\*
-pip install virtualenv
 virtualenv venv
 .\venv\Scripts\pip.exe install -r requirements.txt --target=.\venv\Lib\site-packages --upgrade
 .\venv\Scripts\python.exe -m pytest -n 2  --alluredir=./results --reruns 3 -p no:faulthandler -k "test" --tb=long
