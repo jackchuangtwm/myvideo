d4016dd9564fa7519a8023eefad9ceb8b543cd7f
JackChuang <JackChunag@taiwanmobile.com>
2024-03-08 10:49:47 +0000

    run batch file to activate virtaulenv with full path

--- exec_test.bat
+++ exec_test.bat
@@ -1,4 +1,4 @@
 del /S /Q results\*
-virtualenv venv
+C:\Users\JackChuang\AppData\Local\Programs\Python\Python311\python -m virtualenv venv
 .\venv\Scripts\pip.exe install -r requirements.txt --target=.\venv\Lib\site-packages --upgrade
 .\venv\Scripts\python.exe -m pytest -n 2  --alluredir=./results --reruns 3 -p no:faulthandler -k "test" --tb=long
